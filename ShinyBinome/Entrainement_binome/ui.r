c57fd10c60063744ae8aa96c02d32426f4fce00b
zeliefroment <64363695+zeliefroment@users.noreply.github.com>
2020-05-11 19:44:17 +0200

    idées pour demain

---
+++ ShinyBinome/Entrainement_binome/ui.r
@@ -0,0 +1,16 @@
+library(shiny)
+#Nom/Prénom avec TextInput
+#Âge avec TextInput
+#Sexe avec RadioButtons
+#Nationalité avec selectInput
+#Profession avec textAreaInput()
+#Combien de personnes sous le même toit? avec sliderInput
+#Aimez-vous les iris? avec RadioButtons (yes/no/maybe)
+
+#Afficher tableau iris (-	dataTableOutput ()et renderDataTable()rendre un tableau dynamique, 
+  #montrant un nombre fixe de lignes ainsi que des commandes pour changer les lignes qui sont visibles.
+#plot des espèces en fonction longueur pétale avec PlotOutput
+#barplot des espèces en fonction longueur sépale
+
+#Faire un hisogramme interactif : les valeurs sont entrées par la personnes ?
+  #https://mastering-shiny.org/basic-reactivity.html?
