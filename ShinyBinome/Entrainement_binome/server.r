8384e8f28c170887f46453404a4ab753537a23e0
Louise.Arsac <larsac@etu.isara.fr>
2020-05-12 16:58:00 +0200

    graph sépales

--- ShinyBinome/Entrainement_binome/server.r
+++ ShinyBinome/Entrainement_binome/server.r
@@ -11,4 +11,11 @@ server <- function(input, output, session) {
     u2 <- iris[,5]
     plot(u1~u2, xlab="Espèces", ylab = "Longueur des pétales", main = "Différence de taille des pétales selon l'espèce")
   })
-}
+  
+  output$iris_Plot<- renderPlot({
+    data ("iris")
+    plot (iris$Sepal.Length, iris$Species, main= "Longueur des sepales en fonction de l'espece", xlab= "Longueur sepales", ylab= "Especes")
+    
+  })
+  
+  }
