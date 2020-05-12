4f9bfffc3a4882e272b84d9af1b779b891701001
Louise.Arsac <larsac@etu.isara.fr>
2020-05-12 14:58:37 +0200

    ajout input louise version bien

--- ShinyBinome/Entrainement_binome/ui.r
+++ ShinyBinome/Entrainement_binome/ui.r
@@ -2,11 +2,15 @@ library(datasets)
 library(shiny)
 
 ui <- fluidPage(
+  textInput ("name", "Indiquez votre prenom"),
   textInput("surname","Votre nom de famille :"),
+  dateInput("naissance", "Date de naissance: "),
+  selectInput("nationalite", "Quelle est votre nationalite ? ", state.name),
   textAreaInput("job", "Quelle est votre profession?", rows = 4),
+  radioButtons("sexe", "Indiquez ci-dessous votre sexe", c("Femme", "Homme")),
   sliderInput("toit", "Indiquez le nombre de personne vivant avec vous sous votre foyer", 
               min = 0, max = 15, value = 0),
-  radioButtons("iris", "Aimez-vous les iris", c("yes","no","may be")),
+  radioButtons("iris", "Aimez-vous les iris", c("yes","no","maybe")),
   plotOutput("distPlot")
   
   
