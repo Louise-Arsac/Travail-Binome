f441aa615450339e33cebd6f0fd6027fb88d1a8e
Louise.Arsac <larsac@etu.isara.fr>
2020-05-12 15:18:05 +0200

    sortie tableau

--- ShinyBinome/Entrainement_binome/server.r
+++ ShinyBinome/Entrainement_binome/server.r
@@ -1,3 +1,5 @@
 server <- function(input, output, session) {
 
+  output$static<- renderTable(iris)
+  # output$static <- renderTable(head (iris)) # soit l'un soit l'autre mais le premier est plus complet
 }
