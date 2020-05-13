ba66a1cdee11381c506c5a693acff0cb024cdcd9
zeliefroment <64363695+zeliefroment@users.noreply.github.com>
2020-05-13 15:29:03 +0200

    Deux joueurs

--- ProjetJoueurs/Joueurs/server.r
+++ ProjetJoueurs/Joueurs/server.r
@@ -1,13 +1,19 @@
-tabjoueur <- read.csv2(file = "joueur.csv",header=TRUE,sep=";", dec=".",row.names=1)
+
 
 
 server <- function(input, output, session) {
   output$joueur <- renderText({
-    paste("Joueur1 :",input$name, input$surname, input$age, input$nationalite, input$job, input$sexe)
+    paste("Joueur :",input$name, input$surname, input$age, input$nationalite, input$job, input$sexe)
   })
   
   output$listejoueurs <- renderTable({
-    tabjoueur <- data.frame("Prénom"= input$name, "Nom" =input$surname, "Date de naissance"=input$age, "Nationalité" = input$nationalite, "Profession" = input$job,"Sexe"=input$sexe)
+    tabjoueur <- data.frame("Prénom"= c(input$name1,input$name2), "Nom" =c(input$surname1,input$surname2), "Date de naissance"=c(input$age1,input$age2), "Nationalité" = c(input$nationalite1,input$nationalite2), "Profession" = c(input$job1,input$job2),"Sexe"=c(input$sexe1,input$sexe2))
   })
-} 
   
+  output$info <-renderText({
+    conteneur <- "Réponses :"
+    conteneur <- c(conteneur,input$name)
+  
+})
+} 
+
